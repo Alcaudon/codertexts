e0685184a6b12ef845c6194557619e2651d20530
Luis Barriga <barbaclu@gmail.com>
2018-03-26 13:00:28 +0200

    Finalizamos los permisos  de usuarios y mejoramos algunas detalles  del modelo

--- users/serializers.py
+++ users/serializers.py
@@ -22,3 +22,21 @@ class UserSerializer(serializers.ModelSerializer):
         if self.instance and self.instance.email != data and User.objects.filter(email=data).exists():
             raise ValidationError("El correo de usuario está ya en uso.")
         return data
+
+    def create(self, validated_data):
+        instance = User()
+        return self.update(instance, validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name")
+        instance.last_name = validated_data.get("last_name")
+        instance.username = validated_data.get("username")
+        instance.email = validated_data.get("email")
+        instance.set_password(validated_data.get("password"))
+        instance.image = validated_data.get("image")
+        instance.instagram_user = validated_data.get("instagram_user")
+        instance.twitter_user = validated_data.get("twitter_user")
+        instance.facebook_user = validated_data.get("facebook_user")
+        instance.about_me = validated_data.get("about_me")
+        instance.save()
+        return instance
