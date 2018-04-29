95ee5a5b7579ecd1dbf216d422a64b65afcab278
Luis Barriga <barbaclu@gmail.com>
2018-04-29 18:52:42 +0200

    Creación de punto de acceso para trabajar con los usuarios

--- users/api.py
+++ users/api.py
@@ -1,10 +1,15 @@
-from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView
 
 from users.authentication import TokenAuthentication
 from users.models import User
 from users.permissions import UserPermissions
 from users.serializers import UserSerializer
 
+class UserListAPI(ListAPIView):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [UserPermissions]
 
 class UserCreateAPI(CreateAPIView):
 
