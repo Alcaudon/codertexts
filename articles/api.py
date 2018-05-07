5e60e183d730dea0b6f9b065236941c19972fd6f
VIRIATO <javi.lindo@gmail.com>
2018-05-07 19:12:50 +0200

    Corrección de errores en el API y subida de imagenes relacionadas con una entrada

--- articles/api.py
+++ articles/api.py
@@ -1,39 +1,64 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+
+from articles.permisssions import ArticlePermissions
 from articles.serializers import ArticlesSerializer
 from articles.models import Article
-from users.permissions import UserPermissions
+from users.permissions import UserPermissions, IsOwnerOrReadOnly
 from articles.models import Category
 from articles.serializers import CategoriesSerializer
 from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 
 
 #@method_decorator(csrf_protect, name='dispatch')
 class NewArticleAPI(CreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticlesSerializer
-    permission_classes = [UserPermissions, IsAuthenticated]
+    permission_classes = [ArticlePermissions]
 
 
 class ActionArticleAPI(RetrieveUpdateDestroyAPIView):
 
     queryset = Article.objects.all()
     serializer_class = ArticlesSerializer
-    permission_classes = [UserPermissions]
+    permission_classes = [ArticlePermissions]
 
 
-@method_decorator(csrf_protect, name='dispatch')
+#@method_decorator(csrf_protect, name='dispatch')
 class GetAllArticlesAPI(ListAPIView):
-
-    queryset = Article.objects.all()
+    """Artículos que están publicados y cuya fecha es menor o igual a la de hoy"""
+    now = datetime.now()
+    queryset = Article.objects.filter(pub_date__lte=now).order_by('-pub_date')
     serializer_class = ArticlesSerializer
 
+    def retrieve(self, request, pk=None):
+        if request.user.is_superuser or (request.user.is_authenticated and str(request.user.id) == pk):
+            posts_list = Article.objects.all().order_by('-publish_date')
+        else:
+            now = datetime.now()
+            posts_list = Article.objects.filter(user=pk, pub_date__lte=now).order_by('-pub_date')
+        serializer = ArticlesSerializer(posts_list, many=True)
+        return Response(serializer.data)
 
-class GetAllArticlesByUserAPI(ListAPIView):
+
+class ArticlesByUserAPI(ListAPIView):
+    """Artículos publicados por un usuario"""
     serializer_class = ArticlesSerializer
-    permission_classes = [UserPermissions]
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        now = datetime.now()
+        id_user = self.kwargs.get('id_user')
+        queryset = Article.objects.filter(id_user=id_user,  pub_date__lte=now, status='finalizado')
+        return queryset
+
+
+class OwnArticlesAPI(ListAPIView):
+    """Artículos propios de un usuario"""
+    serializer_class = ArticlesSerializer
+    permission_classes = [UserPermissions, IsAuthenticated]
 
     def get_queryset(self):
         id_user = self.kwargs.get('id_user')
--- users/permissions.py
+++ users/permissions.py
@@ -1,15 +1,14 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class UserPermissions (BasePermission):
-
+class UserPermissions(BasePermission):
     def has_permission(self, request, view):
 
         #  Puede conectarse cualquiera mediante POST para garantizar el alta de nuevos usuarios en el sistema
         if request.method == 'POST':
             return True
 
-        #  Si quieres ver el listado de usuarios completo debes ser superadmin
+        # Si quieres ver el listado de usuarios completo debes ser superadmin
         if request.method == 'GET' and request.user.is_superuser:
             return True
 
@@ -27,3 +26,26 @@ class UserPermissions (BasePermission):
     def has_object_permission(self, request, view, obj):
         # Si soy administrador o pido los datos de mi propio usuario, entonces puedo verlos
         return request.user == obj or request.user.is_superuser
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    message = 'You must be the owner of this object.'
+    my_safe_method = ['GET']
+
+    def has_permission(self, request, view):
+        if request.method in self.my_safe_method:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
+
+class IsSuperUser(BasePermission):
+    message = 'You must be a superuser.'
+
+    def has_permission(self, request, view):
+        return request.user.is_superuser
